d5096b3f7085851aedcdb6a60b4307edaa618c8e
nickholmstyle <nick416holm@protonmail.com>
2022-09-27 18:24:37 +0000

    new seeds

--- db/seeds/04_favourites.sql
+++ db/seeds/04_favourites.sql
@@ -21,16 +21,14 @@ VALUES (2, 1);
 INSERT INTO favourites (user_id, map_id)
 VALUES (2, 2);
 
-INSERT INTO favourites (user_id, map_id)
-VALUES (2, 3);
+
 
 --User with id 3 favaoutites all 3 sample maps
 
 INSERT INTO favourites (user_id, map_id)
 VALUES (3, 1);
 
-INSERT INTO favourites (user_id, map_id)
-VALUES (3, 2);
 
-INSERT INTO favourites (user_id, map_id)
-VALUES (3, 3);
+
+
+
